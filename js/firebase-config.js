3654740a05cbefadd29994da3e9014127b37e30b
leonmarinho95 <leonmarinho95@gmail.com>
2026-06-19 15:43:04 -0300

    Add files via upload

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,19 +1,9 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyDkkqV9U9YUyFnS2i-JuBJJAfjsQ2Wj72s",
-    authDomain: "cerebro-ef33c.firebaseapp.com",
-    projectId: "cerebro-ef33c",
-    storageBucket: "cerebro-ef33c.firebasestorage.app",
-    messagingSenderId: "977554209406",
-    appId: "1:977554209406:web:e87b15da921ce76027762f"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-</script>
+// firebase-config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyDkkqV9U9YUyFnS2i-JuBJJAfjsQ2Wj72s",
+  authDomain: "cerebro-ef33c.firebaseapp.com",
+  projectId: "cerebro-ef33c",
+  storageBucket: "cerebro-ef33c.firebasestorage.app",
+  messagingSenderId: "977554209406",
+  appId: "1:977554209406:web:e87b15da921ce76027762f"
+};
