0350b79294de67a1ad8e27851bd299c8b61183a3
leonmarinho95 <leonmarinho95@gmail.com>
2026-06-19 16:05:12 -0300

    Add files via upload

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -7,10 +7,10 @@
 // mantenha-os neste arquivo isolado para facilitar manutenção e troca de ambiente.
 
 export const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_PROJETO.firebaseapp.com",
-  projectId: "SEU_PROJETO",
-  storageBucket: "SEU_PROJETO.appspot.com",
-  messagingSenderId: "SEU_SENDER_ID",
-  appId: "SEU_APP_ID",
+  apiKey: "AIzaSyDkkqV9U9YUyFnS2i-JuBJJAfjsQ2Wj72s",
+  authDomain: "cerebro-ef33c.firebaseapp.com",
+  projectId: "cerebro-ef33c",
+  storageBucket: "cerebro-ef33c.firebasestorage.app",
+  messagingSenderId: "977554209406",
+  appId: "1:977554209406:web:e87b15da921ce76027762f"
 };
